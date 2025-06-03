c2242c516d19d892d4fa9a60cc2a06df92da5f8d
SidHerod <simonherod22@Hotmail.co.uk>
2025-06-03 11:35:02 +1000

    Update AgeGuessingScreen.tsx
    test

--- components/AgeGuessingScreen.tsx
+++ components/AgeGuessingScreen.tsx
@@ -151,7 +151,30 @@ const AgeGuessingScreen: React.FC = () => {
     );
   }
 
-  return <div>{/* ...rest of original UI code... */}</div>;
-};
-
-export default AgeGuessingScreen;
+  return (
+  <div className="text-center p-10">
+    <h1 className="text-3xl font-bold mb-4">Guess Their Age</h1>
+    {currentUserToGuess && (
+      <img
+        src={currentUserToGuess.photoBase64}
+        alt="User to guess"
+        className="w-48 h-48 object-cover rounded-full mx-auto mb-4"
+      />
+    )}
+    <p className="text-lg mb-2">Your Guess: {currentGuessValue}</p>
+    <input
+      type="range"
+      min="10"
+      max="100"
+      value={currentGuessValue}
+      onChange={handleSliderChange}
+      onMouseUp={handleSliderRelease}
+      className="w-full"
+    />
+    {animatedFeedback && (
+      <p className={`mt-4 text-2xl font-bold ${animatedFeedback.colorClass}`}>
+        {animatedFeedback.text}
+      </p>
+    )}
+  </div>
+);
